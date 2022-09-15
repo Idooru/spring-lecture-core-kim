766ba064837a76db1cbdf1b14ba73e34df2b6ba5
Idooru <shere1765@gmail.com>
2022-09-15 22:40:37 +0900

    Update OrderApp class that use spring container

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -4,21 +4,22 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         int discountedPrice = order.calculatePrice();
 
         System.out.println("order = " + order);
